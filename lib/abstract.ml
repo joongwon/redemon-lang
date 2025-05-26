17cf3df5e9672b7b8a6a83856f70a76ddfc58a85
joongwon <winterdawnlight@snu.ac.kr>
2025-05-26 16:30:50 +0900

    Split files and add abstract_demo

--- lib/abstract.ml
+++ lib/abstract.ml
@@ -1,80 +1,14 @@
 open Tree.Syntax
+open Demo
 open Texpr
 
-type path = int list
+type abstraction = {
+  sketch : texpr;
+  init : record;
+  steps : (action * record) list;
+}
 
-type edit =
-  | Dup of int
-  | Del of int
-  | Replace of const
-  | Insert of int * tree
-  | SetAttr of string * const option
-
-exception Type_error of string
-
-(* semantics of edit *)
-let apply_do (edit : edit) (t : tree) : tree option =
-  match (edit, t) with
-  | Dup i, Elem ({ children; _ } as t) ->
-      if i >= List.length children then
-        raise (Invalid_argument "Index out of bounds for duplication");
-      (* Duplicate the i-th child *)
-      let children' =
-        List.mapi (fun j c -> if j = i then [ c; c ] else [ c ]) children
-        |> List.concat
-      in
-      Some (Elem { t with children = children' })
-  | Del i, Elem ({ children; _ } as t) ->
-      if i >= List.length children then
-        raise (Invalid_argument "Index out of bounds for deletion");
-      (* Remove the i-th child *)
-      let children' = List.filteri (fun j _ -> j <> i) children in
-      Some (Elem { t with children = children' })
-  | Replace _, Elem _ -> raise (Type_error "Cannot replace Elem with Const")
-  | Replace new_tree, Const _ -> Some (Const new_tree)
-  | Insert (i, new_tree), Elem ({ children; _ } as t) ->
-      if i > List.length children then
-        raise (Invalid_argument "Index out of bounds for insertion");
-      (* Insert new_tree at index i *)
-      let children' =
-        if i = List.length children then children @ [ new_tree ]
-        else
-          List.mapi
-            (fun j c -> if j = i then [ new_tree; c ] else [ c ])
-            children
-          |> List.concat
-      in
-      Some (Elem { t with children = children' })
-  | SetAttr (key, None), Elem ({ attrs; _ } as t) ->
-      (* Remove the attribute if it exists *)
-      let attrs' = List.remove_assoc key attrs in
-      Some (Elem { t with attrs = attrs' })
-  | SetAttr (key, Some value), Elem ({ attrs; _ } as t) ->
-      (* Set or update the attribute *)
-      let attrs' =
-        (key, Tree.Syntax.AttrConst value) :: List.remove_assoc key attrs
-      in
-      Some (Elem { t with attrs = attrs' })
-  | (Dup _ | Del _ | Insert _ | SetAttr _), Const _ ->
-      raise (Type_error "Cannot apply edit to Const")
-
-let rec apply_traverse (path : path) (edit : edit) (t : tree) : tree option =
-  match path with
-  | [] -> apply_do edit t
-  | i :: rest -> (
-      match t with
-      | Elem ({ children; _ } as t) ->
-          if i >= List.length children then
-            raise (Invalid_argument "Index out of bounds for path");
-          (* Traverse to the i-th child and apply the edit *)
-          let children' =
-            List.mapi
-              (fun j c -> if j = i then apply_traverse rest edit c else Some c)
-              children
-            |> List.filter_map Fun.id
-          in
-          Some (Elem { t with children = children' })
-      | Const _ -> raise (Invalid_argument "Cannot traverse into Const"))
+exception Not_supported of string
 
 let aexpr_of_attr_value (v : attr_value) : aexpr =
   match v with AttrFunc l -> AttrFunc l | AttrConst c -> AttrConst (Const c)
@@ -90,8 +24,6 @@ let rec texpr_of_tree (t : tree) : texpr =
           attrs = List.map (fun (k, v) -> (k, aexpr_of_attr_value v)) attrs;
           children = Fixed (List.map texpr_of_tree children);
         }
-
-exception Not_supported of string
 
 let abstract_step (edit : edit) (e : texpr) (env : record) :
     texpr * (record -> record) * record =
@@ -293,16 +225,6 @@ let rec abstract_step_traverse (path : path) (edit : edit) (e : texpr)
       in
       (e', s, env')
 
-type action = Click of label | Input of label * string
-type demo_step = action * (path * edit) list
-type demo = { init : tree; steps : demo_step list }
-
-type abstraction = {
-  sketch : texpr;
-  init : record;
-  steps : (action * record) list;
-}
-
 let init_abstraction (init : tree) : abstraction =
   let sketch = texpr_of_tree init in
   { sketch; init = []; steps = [] }
@@ -320,3 +242,7 @@ let add_step ({sketch; init; steps} : abstraction) ((action, edits) : demo_step)
   in
   let steps' = (List.map (fun (a, e) -> (a, s e)) steps) @ [(action, env')] in
   { sketch = sketch'; init = s init; steps = steps' }
+
+let abstract_demo ({init; steps} : demo) : abstraction =
+  let init = init_abstraction init in
+  List.fold_left add_step init steps
---
+++ lib/demo.ml
@@ -0,0 +1,81 @@
+open Tree.Syntax
+
+type path = int list
+
+type edit =
+  | Dup of int
+  | Del of int
+  | Replace of const
+  | Insert of int * tree
+  | SetAttr of string * const option
+
+type action = Click of label | Input of label * string
+type demo_step = action * (path * edit) list
+type demo = { init : tree; steps : demo_step list }
+
+exception Type_error of string
+
+(* semantics of edit *)
+let apply_do (edit : edit) (t : tree) : tree option =
+  match (edit, t) with
+  | Dup i, Elem ({ children; _ } as t) ->
+      if i >= List.length children then
+        raise (Invalid_argument "Index out of bounds for duplication");
+      (* Duplicate the i-th child *)
+      let children' =
+        List.mapi (fun j c -> if j = i then [ c; c ] else [ c ]) children
+        |> List.concat
+      in
+      Some (Elem { t with children = children' })
+  | Del i, Elem ({ children; _ } as t) ->
+      if i >= List.length children then
+        raise (Invalid_argument "Index out of bounds for deletion");
+      (* Remove the i-th child *)
+      let children' = List.filteri (fun j _ -> j <> i) children in
+      Some (Elem { t with children = children' })
+  | Replace _, Elem _ -> raise (Type_error "Cannot replace Elem with Const")
+  | Replace new_tree, Const _ -> Some (Const new_tree)
+  | Insert (i, new_tree), Elem ({ children; _ } as t) ->
+      if i > List.length children then
+        raise (Invalid_argument "Index out of bounds for insertion");
+      (* Insert new_tree at index i *)
+      let children' =
+        if i = List.length children then children @ [ new_tree ]
+        else
+          List.mapi
+            (fun j c -> if j = i then [ new_tree; c ] else [ c ])
+            children
+          |> List.concat
+      in
+      Some (Elem { t with children = children' })
+  | SetAttr (key, None), Elem ({ attrs; _ } as t) ->
+      (* Remove the attribute if it exists *)
+      let attrs' = List.remove_assoc key attrs in
+      Some (Elem { t with attrs = attrs' })
+  | SetAttr (key, Some value), Elem ({ attrs; _ } as t) ->
+      (* Set or update the attribute *)
+      let attrs' =
+        (key, Tree.Syntax.AttrConst value) :: List.remove_assoc key attrs
+      in
+      Some (Elem { t with attrs = attrs' })
+  | (Dup _ | Del _ | Insert _ | SetAttr _), Const _ ->
+      raise (Type_error "Cannot apply edit to Const")
+
+let rec apply_traverse (path : path) (edit : edit) (t : tree) : tree option =
+  match path with
+  | [] -> apply_do edit t
+  | i :: rest -> (
+      match t with
+      | Elem ({ children; _ } as t) ->
+          if i >= List.length children then
+            raise (Invalid_argument "Index out of bounds for path");
+          (* Traverse to the i-th child and apply the edit *)
+          let children' =
+            List.mapi
+              (fun j c -> if j = i then apply_traverse rest edit c else Some c)
+              children
+            |> List.filter_map Fun.id
+          in
+          Some (Elem { t with children = children' })
+      | Const _ -> raise (Invalid_argument "Cannot traverse into Const"))
+
